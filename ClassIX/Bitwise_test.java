0bc5a0fee248a0813598e983e8c712effba7cebc
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-04-29 10:08:10 +0530

    Add files via upload

---
+++ ClassIX/Bitwise_test.java
@@ -0,0 +1,19 @@
+package ClassIX;
+
+class Bitwise_test
+{public static void main()
+    {
+        int a = 29;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        
+        int p = 12 >>> 3;
+        int q = 0;
+        int r = 0;
+        int s = 0;
+        int t = 0;
+        
+        System.out.println(p + " " + q + " " + r + " " + s + " " + t);
+    }
+}
