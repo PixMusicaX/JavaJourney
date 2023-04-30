1efc36d7ecc6051aa06107716cb0621ed348aa74
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-04-30 11:33:26 +0530

    Add files via upload

---
+++ ClassIX/Coprime.java
@@ -0,0 +1,29 @@
+package ClassIX;
+
+import java.util.Scanner;
+class Coprime
+{public int HCF(int a, int b)
+    {int r = a % b;
+        while (r != 0 )
+        {a = b; 
+            b = r;
+            r = a % b;
+        }
+        return b;
+    }
+    
+    public static void main(String args[])
+    {int a, b, r;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        a = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        b = sc.nextInt();
+        Coprime ob = new Coprime();
+        r = ob.HCF(a,b);
+        if (r==1)
+        System.out.println(a + " and " + b + " are coprime");
+        else
+        System.out.println(a + " and " + b + " are not coprime");
+    }
+}
