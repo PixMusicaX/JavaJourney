b895155bb640cc9e16832ea3100717b15625ca5e
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-01 15:37:05 +0530

    Add files via upload

---
+++ ClassIX/Digit_Frequency.java
@@ -0,0 +1,27 @@
+package ClassIX;
+
+import java.util.Scanner;
+class Digit_Frequency
+{public static void main()
+    
+    {Scanner sc = new Scanner(System.in);
+        Digit_Frequency ob = new Digit_Frequency();
+        int n, c, k;
+        System.out.print("Enter the number: ");
+        n = sc.nextInt();
+        int con = 0;
+        c = n;
+        System.out.println("In " + n + ",");
+        while (con < 10)
+        {k = Fun.Freq(n, con);
+            if(k != 0)
+              System.out.println(con + " returns " + k + " times");
+            con++;
+            
+        }
+    }
+}
+
+
+
+
