e57def209ae02b274153124e9ce88ee20fa98452
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-01 15:37:36 +0530

    Add files via upload

---
+++ ClassIX/Div_by_2_and_7_nestedifelse.java
@@ -0,0 +1,18 @@
+package ClassIX;
+
+class Div_by_2_and_7_nestedifelse
+{public void check(int n)
+    {if (n%2 == 0)
+        {if (n%7 == 0)
+            System.out.println("Divisible by 2 and 7");
+            else
+            System.out.println("Divisible by 2 and not by 7");
+        }
+        else
+        { if (n%7 == 0)
+          System.out.println("Divisible by 7 and not by 2");
+          else
+          System.out.println("Not divisible by 2 and 7");
+        }
+     }
+}
