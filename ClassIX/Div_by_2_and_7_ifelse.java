a7eb9e70c4aecf12adf3558c55c1d91bee060570
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-01 15:37:23 +0530

    Add files via upload

---
+++ ClassIX/Div_by_2_and_7_ifelse.java
@@ -0,0 +1,14 @@
+package ClassIX;
+
+class Div_by_2_and_7_ifelse
+{public void check(int n)
+    {if (n%2 == 0 && n%7 == 0)
+        System.out.println("Divisible by 2 and 7");
+        else if (n%2 == 0 && n%7 != 0)
+        System.out.println("Divisible by 2 and not by 7");
+        else if (n%2 != 0 && n%7 == 0)
+        System.out.println("Divisible by 7 and not by 2");
+        else
+  System.out.println("Not divisible by 2 and 7");
+}
+}
