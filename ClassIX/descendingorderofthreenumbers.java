6807e703c21d7ba5b5d43395f4f6f51515c3c7d7
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-01 15:36:48 +0530

    Add files via upload

---
+++ ClassIX/descendingorderofthreenumbers.java
@@ -0,0 +1,10 @@
+package ClassIX;
+
+class descendingorderofthreenumbers
+{public void display(int a, int b, int c)
+    {int max = Math.max(a, Math.max(b,c));
+        int min = Math.min(a, Math.min(b,c));
+        int mid = a + b+ c - max - min;
+        System.out.println("The descending order is: " + max + " > " + mid + " > " + min);
+    }
+}
