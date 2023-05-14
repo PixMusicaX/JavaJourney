4b447412347bbcbda48efeebc9e8336ee871f43d
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-14 19:49:04 +0530

    Add files via upload

---
+++ ClassIX/Interchange.java
@@ -0,0 +1,11 @@
+package ClassIX;
+
+class Interchange
+{public void calculate(int n1, int n2)
+    {System.out.println("The numbers before swapping are: " +n1 +" " + n2);
+        n1 = n1 + n2;
+        n2 = n1 - n2;
+        n1 = n1 - n2;
+        System.out.println("The numbers after swapping are: " + n1 + " " + n2);
+    }
+}
