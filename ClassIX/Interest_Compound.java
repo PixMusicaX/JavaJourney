2440543cc0d0574471b3507b1ec6158d07614de3
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-14 19:49:17 +0530

    Add files via upload

---
+++ ClassIX/Interest_Compound.java
@@ -0,0 +1,12 @@
+package ClassIX;
+
+class Interest_Compound
+//enter principal, rate and time. Get the amount!
+{double a;
+    public void display(double p, double r, double t)
+    {a = p*Math.pow((1 + r/100),t)*1.00;
+        a = a * 1.00;
+        System.out.println("Your interest is: " + (a - p));
+        System.out.println("The amount you need to pay is " + a);
+    }
+}
