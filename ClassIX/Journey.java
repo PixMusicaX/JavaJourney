bf98fd2b87b94aa6ea9eb9d1740d395d4a652c59
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-15 13:21:51 +0530

    Add files via upload

---
+++ ClassIX/Journey.java
@@ -0,0 +1,10 @@
+package ClassIX;
+
+class Journey
+{public void calculate(int hr, int min)
+    {int time = 2* (min + (hr*60));
+        int rhr = time/60;
+        int rmin = time%60;
+        System.out.println("Total time taken: " + rhr + " hours and " + rmin + " minutes");
+    }
+}
