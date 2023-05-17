f6ebdab67e013c59caf93b34cf16400779ca8e63
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-17 10:25:28 +0530

    Add files via upload

---
+++ ClassIX/Krishnamurthy.java
@@ -0,0 +1,29 @@
+package ClassIX;
+
+import java.util.Scanner;
+class Krishnamurthy
+{public int fact(int n)
+    {int fact = 1;
+        for (int i = 1; i <= n; i++)
+        fact *= n;
+        return fact;
+    }
+    
+    public static void main(String args[])
+    {int n, s = 0, c, d;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        n = sc.nextInt();
+        Krishnamurthy ob = new Krishnamurthy();
+        c = n;
+        while(c != 0)
+        {d = c % 10;
+            s += ob.fact(d);
+            c = c / 10;
+        }
+        if(n == s)
+        System.out.print(n + " is a krishnamurthy number");
+        else
+        System.out.print(n + " is a krishnamurthy number");
+    }
+}
