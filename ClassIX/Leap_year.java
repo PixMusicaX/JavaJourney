730b77e1bc66bca72fc541cf8d17781a9c58aeb8
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-18 19:40:40 +0530

    Add files via upload

---
+++ ClassIX/Leap_year.java
@@ -0,0 +1,13 @@
+package ClassIX;
+
+class Leap_year
+{
+public void display(int y) 
+    {if (y%100 == 0)
+        System.out.println(y + " is a leap year");
+        else if (y%4 == 0 && y%100!= 0)
+        System.out.println(y + " is a leap year");
+        else
+        System.out.println(y + " is not a leap year"); 
+    }
+}
