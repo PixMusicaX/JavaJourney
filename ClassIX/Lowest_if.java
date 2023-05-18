f827af6b6b48be6754323f6aeac8ef68b87d2ee1
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-18 19:41:05 +0530

    Add files via upload

---
+++ ClassIX/Lowest_if.java
@@ -0,0 +1,12 @@
+package ClassIX;
+
+class Lowest_if
+{public void display(int a, int b, int c)
+    {if (a<b && a<c)
+        System.out.println(a + " is the lowest number");
+        else if (b<c)
+        System.out.println(b + " is the lowest number");
+        else
+        System.out.println(c + " is the lowest number");
+    }
+}
