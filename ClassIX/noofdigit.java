dc72ca47acc0709d9fbde6e7e1089012713550d5
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-21 10:54:47 +0530

    Add files via upload

---
+++ ClassIX/noofdigit.java
@@ -0,0 +1,26 @@
+package ClassIX;
+
+class noofdigit
+{//'noofdogit' is the code name of "no. of digit"
+    //this code emplies for positive numbers only.
+public void display(int n)
+{if (n < 10)
+    System.out.println(n + " is a 1 digit number");
+    else if (n < 100)
+    System.out.println(n + " is a 2 digit number");
+    else if (n < 1000)
+    System.out.println(n + " is a 3 digit number");
+    else if (n < 10000)
+    System.out.println(n + " is a 4 digit number");
+    else if (n < 100000)
+    System.out.println(n + " is a 5 digit number");
+    else if (n < 1000000)
+    System.out.println(n + " is a 6 digit number");
+    else if (n < 10000000)
+    System.out.println(n + " is a 7 digit number");
+    else if (n > 9999999)
+    System.out.println(n + " is a long digit number");
+    else
+    System.out.println(n + " is a no digit number");
+}
+}
