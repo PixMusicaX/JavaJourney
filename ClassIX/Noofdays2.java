9f0e110bc84fa3724fdd723d17ee2cd433c52b0c
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-21 10:54:19 +0530

    Add files via upload

---
+++ ClassIX/Noofdays2.java
@@ -0,0 +1,12 @@
+package ClassIX;
+
+class Noofdays2
+{public void calculate(int d)
+    {int year = d/365;
+        int day = d%365;
+        int month = day/30;
+        day = day%30;
+        System.out.println(d + " days are " + year + " year(s) " + month + " month(s) and " + day + " days.");
+    }
+}
+        
