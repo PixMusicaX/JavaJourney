6e207f871ffa2444aa4f921f78931a1ebfff32ad
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-05-21 10:54:07 +0530

    Add files via upload

---
+++ ClassIX/Next_Prime.java
@@ -0,0 +1,16 @@
+package ClassIX;
+
+import java.util.Scanner;
+class Next_Prime
+{
+    public static void main(String args[])
+    {int n;
+        Scanner sc = new Scanner(System.in);
+        Next_Prime ob = new Next_Prime();
+        System.out.print("Enter the number: ");
+        int a = sc.nextInt();
+        int v = Fun.nextPrime(a);
+        System.out.println("The next prime number is " + v);
+    }
+}
+        
