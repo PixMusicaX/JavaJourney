a7a3b51d47af1b312eaad186b584a1954a5100bd
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-06 20:51:33 +0530

    Add files via upload

---
+++ ClassIX/PI.java
@@ -0,0 +1,13 @@
+package ClassIX;
+
+class PI
+{public void display(String state)//Enter "Give me the value"
+    {if (state == "Give me the value")
+        {double pi = Math.PI;
+         System.out.print("The value of Pi is " + pi);
+        }
+        else
+        System.out.println("Wrong Password");
+    }
+}
+        
