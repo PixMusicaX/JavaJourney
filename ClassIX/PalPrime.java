bad57314e63e69b94945fedc0f02bf1cf3ec208e
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-06 20:50:12 +0530

    Add files via upload

---
+++ ClassIX/PalPrime.java
@@ -0,0 +1,18 @@
+package ClassIX;
+
+import java.util.Scanner;
+class PalPrime
+{public static void main(String args[])
+    {int n; 
+        Scanner sc = new Scanner(System.in);
+        Next_Prime ob2 = new Next_Prime();
+        PalPrime ob1 = new PalPrime();
+        System.out.print("Enter the number: ");
+        n = sc.nextInt();
+        if (Fun.isPal(n) && Fun.isPrime(n))
+        System.out.print(n + " is a Palprime number");
+        else
+        System.out.print(n + " is not a Palprime number");
+    }
+}
+        
