726794eb3f351b34345e4f5152d6303857486395
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-06 20:52:31 +0530

    Add files via upload

---
+++ ClassIX/Pythagorian_Triplex.java
@@ -0,0 +1,13 @@
+package ClassIX;
+
+class Pythagorian_Triplex
+{public void calculate(int a, int b, int c)
+    {int max = Math.max(a, Math.max(b, c));
+        int min = Math.min(a, Math.min(b, c));
+        int mid = a + b + c - max - min;
+        if ((max*max) == min*min + mid*mid)
+           System.out.println("It is a pythagorian triplex");
+           else
+           System.out.println("It is not a pythagorian triplex");
+     }
+}
