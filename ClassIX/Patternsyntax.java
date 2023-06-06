4232e86a016a981ec4fe8ab9dbd8f8f95c9382b1
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-06 20:50:28 +0530

    Add files via upload

---
+++ ClassIX/Patternsyntax.java
@@ -0,0 +1,12 @@
+package ClassIX;
+
+class Patternsyntax
+{public void display()
+    {int i, j;
+        for (i = 1;i <= 5; i++)
+        {for (j = 1;j <= i; j++)
+            System.out.print(j);
+            System.out.println();
+        }
+    }
+}
