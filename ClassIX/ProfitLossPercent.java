66dba321bccb4e3b176a93d19754a1af9f21b612
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-06 20:51:53 +0530

    Add files via upload

---
+++ ClassIX/ProfitLossPercent.java
@@ -0,0 +1,18 @@
+package ClassIX;
+
+class ProfitLossPercent
+//original name in copy(correction) is Buiessness
+{public void calculate(double cp, double sp)
+    {double p,l, pp, lp;
+        if (sp > cp)
+        {p = sp - cp;
+            pp = p/cp*100;
+            System.out.println("Your profit is " + p + " and it's percentage is " + pp);
+        }
+        else
+        {l = cp - sp;
+            lp = l/cp*100;
+            System.out.println("Your loss is " + l + " and it's percentage is " + lp);
+        }
+    }
+}
