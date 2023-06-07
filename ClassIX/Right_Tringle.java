5a3a08fc84220980418dcc33fe2d18eff15c971e
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-07 08:25:53 +0530

    Add files via upload

---
+++ ClassIX/Right_Tringle.java
@@ -0,0 +1,12 @@
+package ClassIX;
+
+class Right_Tringle
+{public void calculate(int b, int h)
+    {double area;
+        double peri;
+    area = 0.5*b*h;
+        peri = b + h + (Math.sqrt(Math.pow(b,2) + Math.pow(h,2)));
+    System.out.println("The perimetre is: " + peri);
+        System.out.println("The area is: " + area);
+    }
+}
