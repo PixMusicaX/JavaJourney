9fcbd5017d4e63cbc968d1181aeae06163c7d40e
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-09 11:15:51 +0530

    Add files via upload

---
+++ ClassIX/Student.java
@@ -0,0 +1,24 @@
+package ClassIX;
+
+/* name of class - Student
+ * member variable - roll, name
+ * member method - default constructor, initialize member variable with your roll name
+ * void show() - ti display roll and name        */
+ 
+ class Student
+ {int roll;
+     String name;
+     public Student()
+     {roll = 21;
+         name = "Pinaki";
+        }
+        
+        public void show()
+        {System.out.println(roll + "\t" + name);
+        }
+        
+        public static void main(String args[])
+        {Student ob = new Student();
+            ob.show();
+        }
+    }
