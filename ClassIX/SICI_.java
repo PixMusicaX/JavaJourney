2010a71cb6575fdb88a05c624cf1d29ee749aca9
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-09 11:14:41 +0530

    Add files via upload

---
+++ ClassIX/SICI_.java
@@ -0,0 +1,19 @@
+package ClassIX;
+
+class SICI_
+{public void calculate(double p, double r, double t, int ch)
+    {double a = 0;
+        switch (ch)
+        {case 1:
+            double si = p*r*t/100.0;
+            a = p + si;
+            break;
+            case 2:
+            a = p*Math.pow((1 + r/100.0),t);
+            break;
+            default:
+            System.out.println("Invalid choice");
+        }
+        System.out.println("Your amount is: " + a);
+    }
+}
