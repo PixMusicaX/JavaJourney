be436609aefa022420076155da4ce3cb6abec9e9
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-09 11:15:29 +0530

    Add files via upload

---
+++ ClassIX/Specialnumber.java
@@ -0,0 +1,14 @@
+package ClassIX;
+
+class Specialnumber
+{public void check(int n)
+    {int a = n/10;
+        int b = n%10;
+        int add = a + b;
+        int mul = a * b;
+        if ((add + mul) == n)
+        System.out.println(n + " is a special number");
+        else
+        System.out.println(n + " is not a special number");
+    }
+}
