f2d0b922eff9d16665cea1e90038a40a5d314c45
Pinaki Pritam Singha <129383302+PixMusicaX@users.noreply.github.com>
2023-06-09 11:15:01 +0530

    Add files via upload

---
+++ ClassIX/Sidesquare_area_peri_dia.java
@@ -0,0 +1,22 @@
+package ClassIX;
+
+class Sidesquare_area_peri_dia
+{public void calc(double s, short ch)
+    {switch(ch)
+        {case 1:
+        double area = s*s;
+        System.out.println("Area is " + area);
+        break;
+        case 2:
+        double peri = 4.0*s;
+        System.out.println("Perimetre is " + peri);
+        break;
+        case 3:
+        double dia = Math.sqrt(2*s*s);
+        System.out.println("Diagonal is " + dia);
+        break;
+        default:
+        System.out.println("Invalid choice");
+    }
+}
+}
